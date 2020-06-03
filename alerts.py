5927d8a9ae29e867673ab033777ab8299ff968e2
eeddaann <edan100@gmail.com>
2020-06-03 10:45:24 +0300

    Update ffprobe command to monitor interlacing

--- alerts.py
+++ alerts.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-ffprobe_command = "signalstats='stat=tout+vrep+brng',deflicker=bypass=1,bitplanenoise"
+#ffprobe_command = "signalstats='stat=tout+vrep+brng',deflicker=bypass=1,bitplanenoise"
+ffprobe_command = "signalstats=stat=tout+vrep+brng,bitplanenoise,idet=half_life=1,deflicker=bypass=1,split[a][b];[a]field=top[a1];[b]field=bottom,split[b1][b2];[a1][b1]psnr[c1];[c1][b2]ssim"
 drop_meta = "| cut -f30- -d ','"
 
 def check_low_signal(report):
@@ -8,6 +9,9 @@ def check_low_signal(report):
 
 def check_high_signal(report):
     return report['avg_Ylow'] < 70
+
+def check_interlace(report):
+    return report['interlace_pct'] > 0.75
 
 ALERTS = [
     {
@@ -23,12 +27,14 @@ ALERTS = [
 ]
 
 def extract_metrics(csv_path):
-    columns = ['ymin', 'ylow', 'yavg', 'yhigh', 'ymax', 'umin', 'ulow', 'uavg', 'uhigh', 'umax', 'vmin', 'vlow', 'vavg', 'vhigh', 'vmax', 'satmin', 'satlow', 'satavg', 'sathigh', 'satmax', 'huemed', 'hueavg', 'ydif', 'udif', 'vdif', 'ybitdepth', 'ubitdepth', 'vbitdepth', 'tout', 'vrep', 'brng', 'luminance', 'new_luminance', 'relative_change', 'bitplanenoise01', 'bitplanenoise11', 'bitplanenoise21']
+    #columns = ['ymin', 'ylow', 'yavg', 'yhigh', 'ymax', 'umin', 'ulow', 'uavg', 'uhigh', 'umax', 'vmin', 'vlow', 'vavg', 'vhigh', 'vmax', 'satmin', 'satlow', 'satavg', 'sathigh', 'satmax', 'huemed', 'hueavg', 'ydif', 'udif', 'vdif', 'ybitdepth', 'ubitdepth', 'vbitdepth', 'tout', 'vrep', 'brng', 'luminance', 'new_luminance', 'relative_change', 'bitplanenoise01', 'bitplanenoise11', 'bitplanenoise21']
+    columns = ['ymin', 'ylow', 'yavg', 'yhigh', 'ymax', 'umin', 'ulow', 'uavg', 'uhigh', 'umax', 'vmin', 'vlow', 'vavg', 'vhigh', 'vmax', 'satmin', 'satlow', 'satavg', 'sathigh', 'satmax', 'huemed', 'hueavg', 'ydif', 'udif', 'vdif', 'ybitdepth', 'ubitdepth', 'vbitdepth', 'tout', 'vrep', 'brng', 'bitplanenoise01', 'bitplanenoise11', 'bitplanenoise21', 'idet_repeated_current_frame', 'idet_repeated_neither', 'idet_repeated_top', 'idet_repeated_bottom', 'idet_single_current_frame', 'idet_single_tff', 'idet_single_bff', 'idet_single_progressive', 'idet_single_undetermined', 'idet_multiple_current_frame', 'idet_multiple_tff', 'idet_multiple_bff', 'idet_multiple_progressive', 'idet_multiple_undetermined', 'luminance', 'new_luminance', 'relative_change', 'psnr_mse_y', 'psnr_psnr_y', 'psnr_mse_u', 'psnr_psnr_u', 'psnr_mse_v', 'psnr_psnr_v', 'psnr_mse_avg', 'psnr_psnr_avg', 'ssim_y', 'ssim_u', 'ssim_v', 'ssim_all', 'ssim_db']
     df = pd.read_csv(csv_path,names=columns)
     return {
-        'avg_Yhigh': df.yhigh.mean(),
-        'avg_Ylow': df.ylow.mean(),
-        'yrang': (df.yhigh-df.ylow).mean(),
-        'urang': (df.uhigh-df.ulow).mean(),
-        'vrang': (df.vhigh-df.vlow).mean()
+        'avg_Yhigh': round(df.yhigh.mean(),4),
+        'avg_Ylow': round(df.ylow.mean(),4),
+        'yrang': round((df.yhigh-df.ylow).mean(),4),
+        'urang': round((df.uhigh-df.ulow).mean(),4),
+        'vrang': round((df.vhigh-df.vlow).mean(),4),
+        'interlace_pct': round((df.idet_multiple_bff.sum() + df.idet_multiple_tff.sum())/(2*len(df)),4)
     }
--- main.py
+++ main.py
@@ -62,7 +62,7 @@ try:
     output_url = config['paths']['output_url'] # set url for sending logs 
     save_csv = config['debugging'].getboolean('save_csv') # should we need to save the csv
     operational_log["video_size_bytes"] = os.path.getsize(video_path) # log video size
-    cmd = ["/bin/sh", "-c", "ffprobe -f lavfi movie=%s,%s %s -show_frames -hide_banner -print_format csv %s > %s"%(video_path,
+    cmd = ["/bin/sh", "-c", '''ffprobe -f lavfi -i "movie=%s,%s" %s -show_frames -hide_banner -print_format csv %s > %s'''%(video_path,
                                                                                                                 alerts.ffprobe_command,
                                                                                                                 config['ffprobe']['interval'],
                                                                                                                 alerts.drop_meta,
