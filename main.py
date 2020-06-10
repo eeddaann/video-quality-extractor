fbd38097a695df21d365be8dd006ecb367c6f1a0
eeddaann <edan100@gmail.com>
2020-06-11 02:01:10 +0300

    Add video path to operational log

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ try:
     config.read(config_path) # read configuration
     output_url = config['paths']['output_url'] # set url for sending logs 
     save_csv = config['debugging'].getboolean('save_csv') # should we need to save the csv
+    operational_log["video_path"] = video_path
     operational_log["video_size_bytes"] = os.path.getsize(video_path) # log video size
     cmd = ["/bin/sh", "-c", '''ffprobe -f lavfi -i "movie=%s,%s" %s -show_frames -hide_banner -print_format csv %s > %s'''%(video_path,
                                                                                                                 alerts.ffprobe_command,
